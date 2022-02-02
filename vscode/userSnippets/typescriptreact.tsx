c77d8b91c2f0699ce494fcba4804f958e9a17b00
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2022-02-02 14:22:45 -0600

    Create typescriptreact.tsx

---
+++ vscode/userSnippets/typescriptreact.tsx
@@ -0,0 +1,12 @@
+{
+	"New Styled Component": {
+		"prefix": "sc",
+		"body": [
+		  "export const ${1} = styled.${2}`",
+		  "",
+		  "`;",
+		],
+		"description": "New styled component."
+	  }
+}
+
